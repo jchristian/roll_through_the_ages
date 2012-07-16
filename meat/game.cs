8e7cbcf8046195d8e60f3d8bfa56109a678b7922
Jonathan Christian <jont.christian@gmail.com>
2012-07-16 12:05:30 -0700

    Initial Commit.

---
+++ meat/game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace meat
+{
+    public class game
+    {
+        public game(IEnumerable<Player> players)
+        { }
+
+        public object current_turn { get; set; }
+        public bool is_over { get; private set; }
+
+        public void start_next_turn()
+        {
+             
+        }
+    }
+
+    public class game_runner
+    {
+        public void play_new_game(IEnumerable<Player> players)
+        {
+            var game = new game(players);
+
+            while(!game.is_over)
+            {
+                game.start_next_turn();
+                display(game.current_turn);
+            }
+        }
+
+        public void display(object model) { }
+    }
+
+    public class Player {}
+}
